6cb17986e69b00d4e8d1f70879d51c926aeec740
Ari Shater <ashater@gmail.com>
2020-12-17 21:50:11 -0500

    hw10

--- homework/brickbreaker.py
+++ homework/brickbreaker.py
@@ -1,15 +1,26 @@
 from turtle import Turtle, Screen
 
 # game settings
-ballspeed = 5
+
+ballspeed_x = 2
+ballspeed_y = 2
 playerspeed = 20
 
 cursor_size = 20
+
+# paddle settings
 player_height = 20
 player_width = 80
 
+# court settings
 court_width = 1000
 court_height = 600
+
+# brick settings
+brick_height = 20
+brick_width = 80
+
+
 
 # Ball setup
 ball = Turtle("circle")
@@ -38,7 +49,7 @@ screen.setup(width=1.0, height=1.0)
 def create_brick(x, y):
     # paddle setup
     b = Turtle("square")
-    b.turtlesize(player_height / cursor_size, player_width / cursor_size)
+    b.turtlesize(brick_height / cursor_size, brick_width / cursor_size)
     b.color("white")
     b.penup()
     b.sety(y)
@@ -67,28 +78,39 @@ def up():
     paddle.sety(y) # set y coordinate of the paddle
 
 def move():
-    global ballspeed
-    x, y = ball.position()
+    global ballspeed_x, ballspeed_y
+    ball_x, ball_y = ball.position()
     pad_x, pad_y = paddle.position()
 
-    # x = x + ballspeed
-    # ball.setx(x)
 
     # here we check if we hit all the breaks
-    if y > all_bricks[0].ycor() - player_height:
-        ballspeed = ballspeed * -1
+    for current_brick in all_bricks:
+        cur_brick_x, cur_brick_y = current_brick.position()
+        cur_brick_x_start = cur_brick_x
+        cur_brick_x_end = cur_brick_x + brick_width
+
+        if ball_y > (cur_brick_y - player_height + ballspeed_y) and (ball_x > cur_brick_x_start and ball_x < cur_brick_x_end):
+            ballspeed_x = ballspeed_x * -1
+            ballspeed_y = ballspeed_y * -1
 
     # if you hit the position of the paddle reverse speed
-    if y < pad_y + player_height + ballspeed:
-        ballspeed = ballspeed * -1
+    if ball_y < (pad_y + player_height + ballspeed_y):
+        ballspeed_y = ballspeed_y * -1.0
+
+    # please create 3 if statements, depending on where you hit on the paddle, you change the speed
+    if ball_x > pad_x and ball_x < pad_x + player_width:
+        ballspeed_x = ballspeed_x * -1
+
+
+
 
 
 
     # if you are good, please add check for bounds
-    y = y - ballspeed
-    ball.sety(y)
-
-
+    ball_y = ball_y - ballspeed_x
+    ball.sety(ball_y)
+    ball_x = ball_x - ballspeed_y
+    ball.setx(ball_x)
 
     screen.ontimer(move, 20)
 
